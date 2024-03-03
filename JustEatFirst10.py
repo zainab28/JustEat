e180325cae94af2a06ea3137158b0bf61c256cb1
zainab28 <78147092+zainab28@users.noreply.github.com>
2024-03-03 19:17:30 +0000

    Update JustEatFirst10.py

--- JustEatFirst10.py
+++ JustEatFirst10.py
@@ -7,7 +7,7 @@ def get_restaurant_data(postcode):
     HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
     response = requests.get(url, headers=HEADERS)
 
-    if response.status_code == 200:
+    if response.status_code == 200: # Check if API is reached successfully
         data = json.loads(response.text)
         return data.get("restaurants")[:10] # Return only the first 10 restaurants
     else:
@@ -17,11 +17,11 @@ def get_restaurant_data(postcode):
 postcode = sys.argv[1] # User passes desired postcode
 restaurant_data = get_restaurant_data(postcode)
 
-if restaurant_data:
-    for restaurant in restaurant_data:
+if restaurant_data: # Check if any restaurants get returned for given postcode
+    for restaurant in restaurant_data: # Go through each restaurant and print the 4 fields 
         cuisines = ""
         print("Name: "+restaurant.get("name"))
-        for cuisine in restaurant.get("cuisines"):
+        for cuisine in restaurant.get("cuisines"): # Go through each cuisine for each restaurant
             cuisines += cuisine.get("name")+","
         if cuisines.endswith(","):
             cuisines = cuisines[:-1]
